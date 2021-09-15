2ecbf9d8972c890901a4bf879543581eb2f734c9
Jonathan Adly <jonathanadly@Jonathans-MacBook-Air.local>
2021-09-15 14:08:58 -0400

    complete project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [quote, setQuotes] = useState([]);
+  const [author, setAuthor] = useState([])
+  const [twitterLink, setLink] = useState("")
+
+  const newQuote = () => {
+    fetch("https://www.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setQuotes(result.content);
+          setAuthor(result.author)
+          setLink(`https://www.twitter.com/intent/tweet?text=${result.content}%0D%0A%0D%0A${result.author}`)
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }
+
+  useEffect(newQuote, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="px-3" id="quote-box">
+        <h1> The Quote </h1>
+        <div id="text">
+
+          <p className="lead">{quote} </p>
+        </div>
+        <div id="author">
+          <p> Author: {author} </p>
+        </div>
+        <button className="btn btn-secondary mx-2" onClick={newQuote} id="new-quote"> New Quote </button>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href={twitterLink}
+          id="tweet-quote"
           target="_blank"
           rel="noopener noreferrer"
+          className="btn btn-primary"
         >
-          Learn React
+          Tweet
         </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+
+
+  }
 }
 
 export default App;
